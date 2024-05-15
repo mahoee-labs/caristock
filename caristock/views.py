b594de92a32c48e808049e947e4de00b92afa6b9
Rafael Pivato <rpivato@gmail.com>
2024-05-15 19:46:23 -0300

    Bluma available locally

--- caristock/views.py
+++ caristock/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
+from transactions.models import Donation
+
 
 def render_home(request):
     context = dict(
         overview_highest_count=123,
         overview_lowest_count=-9,
-        incoming_donations=[],
+        incoming_donations=Donation.objects.all(),
     )
     return render(request, "caristock/home.html", context)
