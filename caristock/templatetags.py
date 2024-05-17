64fa5a031bedf139e72b58218a3eb2c08f8d791b
Rafael Pivato <rpivato@gmail.com>
2024-05-17 16:03:29 -0300

    Transitioning screens

--- caristock/templatetags.py
+++ caristock/templatetags.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse, parse_qs, urlencode, urlunparse, quote
 from django import template
 from django.utils.safestring import mark_safe
 
@@ -25,3 +26,44 @@ def add_form_bulma_classes(html_content):
 @register.filter
 def message_class(message_tags):
     return "is-" + message_tags.replace("error", "danger")
+
+
+@register.filter
+def qkey(url, key):
+    """
+    Adds a key with an empty value to the given URL.
+    """
+    parsed_url = urlparse(url)
+    query_params = parse_qs(parsed_url.query)
+    query_params[key] = [""]
+    encoded_query = urlencode(query_params, doseq=True)
+    return urlunparse(
+        (
+            parsed_url.scheme,
+            parsed_url.netloc,
+            parsed_url.path,
+            parsed_url.params,
+            encoded_query,
+            parsed_url.fragment,
+        )
+    )
+
+
+@register.filter
+def qval(url, value):
+    parsed_url = urlparse(url)
+    if parsed_url.query.endswith("="):
+        new_value = str(value)
+        new_query = parsed_url.query + quote(new_value)
+        return urlunparse(
+            (
+                parsed_url.scheme,
+                parsed_url.netloc,
+                parsed_url.path,
+                parsed_url.params,
+                new_query,
+                parsed_url.fragment,
+            )
+        )
+    else:
+        return url
